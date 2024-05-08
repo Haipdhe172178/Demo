956a9ad17c77bb03e6fb5c343bc242085f162b50
Hai Pham <haipdhe172178@fpt.edu.vn>
2024-05-08 12:58:50 +0700

    Test commit

---
+++ PracticeWithMe/src/java/Controllers/Test.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Controllers;
+
+
+
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+    private int num;
+
+    public Test() {
+    }
+
+    public Test(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    
+}
