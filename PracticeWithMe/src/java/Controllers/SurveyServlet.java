bba8e51165989e5f2594f1af3106470401d2c1c2
Pham Duc Hai <haipdhe172178@fpt.edu.vn>
2024-05-08 13:05:44 +0700

    update servlet

--- PracticeWithMe/src/java/Controllers/SurveyServlet.java
+++ PracticeWithMe/src/java/Controllers/SurveyServlet.java
@@ -55,7 +55,19 @@ public class SurveyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SurveyServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SurveyServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        
     } 
 
     /** 
