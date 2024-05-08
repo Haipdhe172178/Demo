f513109c40a874a9c4b8b5bc7e22e375924604f0
Hai Pham <haipdhe172178@fpt.edu.vn>
2024-05-08 12:53:27 +0700

    Upload project
    Try to upload

---
+++ PracticeWithMe/src/java/Model/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Model;
+
+
+
+import java.util.*;
+import java.lang.*;
+
+public class User {
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", password=" + password + '}';
+    }
+    
+    
+}
---
+++ PracticeWithMe/web/View/Add.jsp
@@ -0,0 +1,29 @@
+<%-- 
+    Document   : Add
+    Created on : Jan 21, 2024, 11:26:08 AM
+    Author     : Hai Pham
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+
+        <h1>SURVEY</h1>
+
+        <form method="post">
+            <label>Name:
+                <input type="text" name="name"><br />
+            </label>
+            <label>Password:
+                <input type="password" name="pass"><br />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+
+    </body>
+</html>
